6f6af4ca20b29bb8a7058f8554d166718c4e5d40
babaric-dev <104625126+babaric-dev@users.noreply.github.com>
2023-01-30 14:03:18 +0800

    Add files via upload

--- app/src/main/java/com/termux/filepicker/TermuxDocumentsProvider.java
+++ app/src/main/java/com/termux/filepicker/TermuxDocumentsProvider.java
@@ -74,7 +74,7 @@ public class TermuxDocumentsProvider extends DocumentsProvider {
             row.add(Root.COLUMN_DOCUMENT_ID, getDocIdForFile(root));
             row.add(Root.COLUMN_SUMMARY, null);
             row.add(Root.COLUMN_FLAGS, Root.FLAG_SUPPORTS_CREATE | Root.FLAG_SUPPORTS_SEARCH | Root.FLAG_SUPPORTS_IS_CHILD);
-            row.add(Root.COLUMN_TITLE, rootstr.replaceAll("^" + TERMUX_INTERNAL_PRIVATE_APP_DATA_DIR_PATH, applicationName));
+            row.add(Root.COLUMN_TITLE, rootstr.replaceAll("^" + TermuxConstants.TERMUX_INTERNAL_PRIVATE_APP_DATA_DIR_PATH, applicationName));
             row.add(Root.COLUMN_MIME_TYPES, ALL_MIME_TYPES);
             row.add(Root.COLUMN_AVAILABLE_BYTES, root.getFreeSpace());
             row.add(Root.COLUMN_ICON, R.mipmap.ic_launcher);
